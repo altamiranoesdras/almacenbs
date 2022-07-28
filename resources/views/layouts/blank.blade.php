5d8210d50ff829a92f09cd84f9d9662517de70b5
Edras Altamirano <altamiranoesdras@gmail.com>
2022-07-27 22:28:06 -0600

    Mejoras blank.blade.php

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title> @yield("htmlheader_title") - {{config('app.name')}} </title>
+    <title> @yield("title_page") - {{config('app.name')}} </title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
