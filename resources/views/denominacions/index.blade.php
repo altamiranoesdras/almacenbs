54c47347fe6c03a9b1fa3781c88121e22f74b864
Edras Altamirano <altamiranoesdras@gmail.com>
2022-08-07 16:20:37 -0600

    Mejoras

--- resources/views/denominacions/index.blade.php
+++ resources/views/denominacions/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title_page',__('Denominacions'))
+@section('title_page',__('Denominaciones'))
 
 @section('content')
 
@@ -9,7 +9,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Denominacions</h1>
+                    <h1>Denominaciones</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-right">
@@ -17,7 +17,7 @@
                             <a class="btn btn-outline-success"
                                 href="{!! route('denominacions.create') !!}">
                                 <i class="fa fa-plus"></i>
-                                <span class="d-none d-sm-inline">{{__('New')}}</span>
+                                <span class="d-none d-sm-inline">Nueva</span>
                             </a>
                         </li>
                     </ol>
@@ -39,7 +39,7 @@
                 </div>
             </div>
             <div class="text-center">
-                
+
             </div>
         </div>
     </div>
