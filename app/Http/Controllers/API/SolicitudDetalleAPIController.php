aaad914caf5983c19a8c9273aebd6125482024d4
Edras Altamirano <altamiranoesdras@gmail.com>
2023-02-22 12:19:20 -0600

    No validar stock de insumos al solicitar requisición

--- app/Http/Controllers/API/SolicitudDetalleAPIController.php
+++ app/Http/Controllers/API/SolicitudDetalleAPIController.php
@@ -62,9 +62,9 @@ class SolicitudDetalleAPIController extends AppBaseController
          */
         $item= Item::find($request->item_id);
 
-        if($item->inventariable && $item->stock_total < $request->cantidad_solicitada){
-            return $this->sendError('El stock del articulo no alcanza');
-        }
+//        if($item->inventariable && $item->stock_total < $request->cantidad_solicitada){
+//            return $this->sendError('El stock del articulo no alcanza');
+//        }
 
 
         /** @var SolicitudDetalle $solicitudDetalle */
