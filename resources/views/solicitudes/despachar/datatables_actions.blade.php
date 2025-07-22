634549a9297a78a46862355b45d0434c8f74c2f0
WilianMarroquin <githubwiliangit@gmail.com>
2025-07-22 10:42:01 -0600

    Cambio de clases en los botones de acción en listado de solicitudes para despachar.

--- resources/views/solicitudes/despachar/datatables_actions.blade.php
+++ resources/views/solicitudes/despachar/datatables_actions.blade.php
@@ -1,33 +1,49 @@
 @can('Ver Requisición')
     <button  type="button"
-             class="btn btn-secondary btn-xs"
+             class='btn btn-icon rounded-circle btn-flat-primary'
              data-bs-toggle="modal"
              data-bs-target="#modal-detalles-{{ $id }}"
-             title="Ver detalles">
+             title="Ver detalles"
+             data-toggle="tooltip"
+    >
         <i class="fa fa-eye"></i>
     </button>
 @endcan
 
 @if($solicitud->puedeEditar())
     @can('Editar Requisición')
-        <a href="{{ route('solicitudes.edit', $solicitud->id) }}" class='btn btn-icon btn-flat-info rounded-circle' data-toggle="tooltip" title="Editar">
+        <a
+            href="{{ route('solicitudes.edit', $solicitud->id) }}"
+            class='btn btn-icon btn-flat-info rounded-circle'
+            data-toggle="tooltip"
+            title="Editar"
+        >
             <i class="fa fa-edit"></i>
         </a>
     @endcan
 @endif
 
 @if($solicitud->puedeAnular())
-
-
-    <a href="{{ route('solicitudes.despachoPdf', $id) }}"  class='btn btn-primary btn-xs' data-toggle="tooltip" title="Imprimir" target="_blank">
+    <a
+        href="{{ route('solicitudes.despachoPdf', $id) }}"
+        class='btn btn-icon rounded-circle btn-flat-primary'
+        data-toggle="tooltip"
+        title="Imprimir"
+        target="_blank"
+    >
         <i class="fa fa-file-pdf"></i>
     </a>
 
     @can('Anular Requisición')
-        <a href="#" onclick="deleteItemDt(this)" data-id="{{$solicitud->id}}" data-toggle="tooltip" title="Anular" class='btn btn-outline-danger btn-xs'>
+        <a
+            href="#" onclick="deleteItemDt(this)"
+            data-id="{{$solicitud->id}}"
+            data-toggle="tooltip"
+            title="Anular"
+            class='btn btn-icon rounded-circle btn-flat-danger'
+        >
             <i class="fa fa-undo-alt"></i>
         </a>
-
 
         <form action="{{ route('solicitudes.anular', $solicitud->id)}}" method="POST" id="delete-form{{$solicitud->id}}">
             @method('POST')
