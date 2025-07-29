f81ccb0235d5b1c53b0da4d4c3e761f6c6222766
Esdras Altamirano <altamiranoesdras@gmail.com>
2025-07-29 12:11:07 -0600

    Fix UsersTableSeeder.php

--- resources/views/livewire/mode-layout/mode-layout.blade.php
+++ resources/views/livewire/mode-layout/mode-layout.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item d-none d-lg-block" wire:click="changeMode" wire:ignore>
     <a class="nav-link nav-link-style">
-        <i class="ficon" data-feather="{{ $theme->value == 'light-layout' ? 'moon' : 'sun'}}"></i>
+        <i class="ficon" data-feather="{{ ($theme->value ?? false) == 'light-layout' ? 'moon' : 'sun'}}"></i>
     </a>
 </li>
