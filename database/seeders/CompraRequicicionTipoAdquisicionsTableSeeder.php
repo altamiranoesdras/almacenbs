1ade1eb49e766df45cdc718d956455a32665ee7a
Esdras Altamirano <altamiranoesdras@gmail.com>
2025-08-26 08:26:30 -0600

    Fix

--- database/seeders/CompraRequicicionTipoAdquisicionsTableSeeder.php
+++ database/seeders/CompraRequicicionTipoAdquisicionsTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class CompraRequicicionTipoadquisicionesTableSeeder extends Seeder
+class CompraRequicicionTipoAdquisicionsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
