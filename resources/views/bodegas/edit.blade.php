49563103724013c7b396d6fd214e4d7cefe44169
Esdras Altamirano <altamiranoesdras@gmail.com>
2025-10-16 10:55:23 -0600

    Migración automática con ia a Bs5

--- resources/views/bodegas/edit.blade.php
+++ resources/views/bodegas/edit.blade.php
@@ -12,7 +12,7 @@
                     <h1>{{__('Edit Bodega')}}</h1>
                 </div>
                 <div class="col">
-                    <a class="btn btn-outline-info float-right"
+                    <a class="btn btn-outline-info float-end"
                        href="{{route('bodegas.index')}}">
                         <i class="fa fa-list" aria-hidden="true"></i>&nbsp;<span class="d-none d-sm-inline">{{__('List')}}</span>
                     </a>
@@ -36,7 +36,7 @@
                             @include('bodegas.fields')
 
                             <!-- Submit Field -->
-                            <div class="col-sm-12 mb-1 text-right">
+                            <div class="col-sm-12 mb-1 text-end">
                                 <a href="{!! route('bodegas.index') !!}" class="btn btn-outline-secondary round me-1">
                                     Cancelar
                                 </a>
