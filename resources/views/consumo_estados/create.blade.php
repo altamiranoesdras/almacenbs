ea8ead42f38ba967a6297c2b5fdf1b1bc174f216
Esdras Altamirano <altamiranoesdras@gmail.com>
2025-10-16 11:03:12 -0600

    Migración automática con ia a Bs5

--- resources/views/consumo_estados/create.blade.php
+++ resources/views/consumo_estados/create.blade.php
@@ -12,7 +12,7 @@
                     <h1>{{__('New Consumo Estado')}}</h1>
                 </div>
                 <div class="col ">
-                    <a class="btn btn-outline-info float-right"
+                    <a class="btn btn-outline-info float-end"
                        href="{{route('consumoEstados.index')}}">
                         <i class="fa fa-list" aria-hidden="true"></i>&nbsp;<span class="d-none d-sm-inline">{{__('List')}}</span>
                     </a>
@@ -34,7 +34,7 @@
                             @include('consumo_estados.fields')
 
                             <!-- Submit Field -->
-                            <div class="col-sm-12 mb-1 text-right">
+                            <div class="col-sm-12 mb-1 text-end">
                                 <a href="{!! route('consumoEstados.index') !!}" class="btn btn-outline-secondary round me-1">
                                     Cancelar
                                 </a>
