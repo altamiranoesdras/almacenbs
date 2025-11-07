22406015b7d472a7ce19846bce24971cc264c0d9
Esdras Altamirano <altamiranoesdras@gmail.com>
2025-11-07 14:32:59 -0600

    Editado seeder de opciones

--- database/seeders/OptionsTableSeeder.php
+++ database/seeders/OptionsTableSeeder.php
@@ -1586,6 +1586,23 @@ class OptionsTableSeeder extends Seeder
                 'updated_at' => '2025-11-03 14:15:45',
                 'deleted_at' => NULL,
             ),
+            92 =>
+            array (
+                'id' => 96,
+                'option_id' => 54,
+                'nombre' => 'Mis existencias',
+                'ruta' => 'reportes.mis.existencias',
+                'descripcion' => NULL,
+                'icono_l' => 'fa-circle-notch',
+                'icono_r' => NULL,
+                'orden' => 0,
+                'color' => '2',
+                'recursos' => 0,
+                'dev' => 0,
+                'created_at' => '2025-11-07 14:30:08',
+                'updated_at' => '2025-11-07 14:30:08',
+                'deleted_at' => NULL,
+            ),
         ));
 
         $this->generarColores();
