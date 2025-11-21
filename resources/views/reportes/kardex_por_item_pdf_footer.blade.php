fc63c4c38b3ea4f589eecb64e9c9c25b623e3dd4
Esdras Altamirano <altamiranoesdras@gmail.com>
2025-11-20 21:18:05 -0600

    Mejoras pdf kardex

--- resources/views/reportes/kardex_por_item_pdf_footer.blade.php
+++ resources/views/reportes/kardex_por_item_pdf_footer.blade.php
@@ -1,12 +1,7 @@
 
 <table  style="width: 100%;" border="0" >
-    <tr style="font-size: 12px; text-align: center;">
-
-
-        <td style="width: 194.5mm"></td>
-        <td style="width: 16mm"></td>
-        <td style="width: 17.5mm"></td>
-        <td style="width: 22mm">{{$siguienteFolio ?? ""}}</td>
+    <tr style="font-size: 12px; text-align: right;">
+        <td style="width: 22mm">Saldos pasan a tarjeta: {{$siguienteFolio ?? ""}}</td>
     </tr>
 </table>
 
