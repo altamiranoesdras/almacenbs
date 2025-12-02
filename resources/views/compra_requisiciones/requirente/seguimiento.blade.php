20884b53a44b3aeee25375aa9205ddb76d988237
WilianMarroquin <githubwiliangit@gmail.com>
2025-12-01 23:06:33 -0600

    Mejoras en la funcionalidad.

--- resources/views/compra_requisiciones/requirente/seguimiento.blade.php
+++ resources/views/compra_requisiciones/requirente/seguimiento.blade.php
@@ -70,8 +70,7 @@
                                             </button>
                                         @else
 
-                                            <button type="button" class="btn btn-outline-info round"
-                                                    data-bs-toggle="modal"
+                                            <button type="button" class="btn btn-outline-info round" data-bs-toggle="modal"
                                                     data-bs-target="#modalImprimir">
                                                 Ver PDF Firmado
                                             </button>
@@ -213,7 +212,24 @@
             </div>
         </div>
     </div>
+    <div class="modal fade" id="pdfModal" tabindex="-1" aria-labelledby="pdfModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl"> <!-- modal-xl para que sea grande -->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="pdfModalLabel">Vista previa del PDF</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+                </div>
+                <div class="modal-body p-0">
+                    <!-- Aquí va el visor PDF -->
+                    <div class="ratio ratio-16x9">
+                        <iframe src="{{ session('rutaArchivoFirmado') }}"
+                                frameborder="0"></iframe>
 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('scripts')
